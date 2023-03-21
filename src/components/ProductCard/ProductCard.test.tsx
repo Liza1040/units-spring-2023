ce6959b980e8357213913f3d80f681a4017e75c9
Liza1040 <maxsimova2002eliza@yandex.ru>
2023-03-21 23:39:51 +0300

    Добавлены проверки к продуктовой карточке

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -3,7 +3,10 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ProductCard } from './ProductCard';
 import { Product } from '../../types';
-jest.mock('../../utils');
+jest.mock('../../utils/getPrice', () => ({
+    __esModule: true,
+    getPrice: jest.fn(() => '1 000 ₽')
+}));
 
 const defaultProduct: Product = {
     id: 2, 
@@ -27,4 +30,22 @@ describe('ProductCard test', () => {
         
         expect(rendered.asFragment()).toMatchSnapshot();
     });
+
+    it('тексты рендарятся в нужных местах', () => {
+        const rendered = render(<ProductCard {...defaultProduct} />);
+
+        expect(rendered.getByText('Костюм гуся')).toHaveClass(
+            'product-card__name'
+        );
+        expect(rendered.getByText('Запускаем гуся, работяги')).toHaveClass(
+            'product-card__description'
+        );
+        expect(rendered.getByText('1 000 ₽')).toHaveClass(
+            'product-card__price'
+        );
+        expect(rendered.getByText('Одежда')).toHaveClass(
+            'product-card__category'
+        );
+    });
+
 });
